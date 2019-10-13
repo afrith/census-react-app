7961c16e4c4010fd981845a8cd573c3d16fc1aab
Adrian Frith <adrian@adrianfrith.com>
2019-10-13 15:29:46 +0200

    Add footer

---
+++ src/presentation/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className='mt-4' style={{ fontSize: '0.8rem', fontStyle: 'italic' }}>
+    This site is developed by <a href="https://adrianfrith.com/">Adrian Frith</a>.
+    This site is not affiliated with <a href="http://www.statssa.gov.za/">Statistics South Africa</a>, nor is it affiliated with my employer.
+    Statistics South Africa is the source of the basic data, while the information displayed results from my own processing of the census data.
+    Population statistics were obtained from the Census 2011 Community Profile Databases, and geographical areas were calculated from the Census 2011 GIS DVD.
+  </div>
+)
+
+export default Footer
--- src/presentation/Layout.js
+++ src/presentation/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import Footer from './Footer'
 
 const Layout = ({ children }) => (
   <Container className='my-3'>
     {children}
+    <Footer />
   </Container>
 )
 
