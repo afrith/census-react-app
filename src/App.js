d744edd2d469fcaf93939631c2890b44b627309e
Adrian Frith <adrian@adrianfrith.com>
2019-10-13 12:33:33 +0200

    Add bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
 import Home from './Home'
 import Place from './Place'
 import './App.css'
 
 const App = () => (
-  <Switch>
-    <Route exact path='/'><Home /></Route>
-    <Route path='/place/:code'><Place /></Route>
-  </Switch>
+  <Container>
+    <Switch>
+      <Route exact path='/'><Home /></Route>
+      <Route path='/place/:code'><Place /></Route>
+    </Switch>
+  </Container>
 );
 
 export default App
