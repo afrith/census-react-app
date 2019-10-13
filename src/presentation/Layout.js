82302d605905b87f9817d536b7b2aa6bd2f80da5
Adrian Frith <adrian@adrianfrith.com>
2019-10-13 15:29:39 +0200

    Move page margin into Layout

--- src/presentation/Layout.js
+++ src/presentation/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 
 const Layout = ({ children }) => (
-  <Container>
+  <Container className='my-3'>
     {children}
   </Container>
 )
