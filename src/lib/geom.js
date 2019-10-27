33864556be43df593a42fb9264473869b2a39e45
Adrian Frith <adrian@adrianfrith.com>
2019-10-27 14:47:11 +0200

    Split out separate `makeFeatureFromPlace` function

--- src/lib/geom.js
+++ src/lib/geom.js
@@ -1,9 +1,11 @@
+export const makeFeatureFromPlace = place => ({
+  type: 'Feature',
+  id: place.code,
+  properties: { code: place.code, name: place.name },
+  geometry: place.geom
+})
+
 export const makeFeatureCollectionFromPlaceArray = placeArray => ({
   type: 'FeatureCollection',
-  features: placeArray.map(c => ({
-    type: 'Feature',
-    id: c.code,
-    properties: { code: c.code, name: c.name },
-    geometry: c.geom
-  }))
+  features: placeArray.map(makeFeatureFromPlace)
 })
