3ae6b276580591d2358785a85bc912af4484f13f
Adrian Frith <adrian@adrianfrith.com>
2020-05-23 01:24:20 +0200

    Add warning message referring to 2011 site.

--- src/presentation/Layout.js
+++ src/presentation/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Footer from './Footer'
+import { WarningMessage } from './alerts'
 
 const Layout = ({ children }) => (
   <Container className='py-3'>
+    <WarningMessage message={<>This site shows results from Census 2001. You might be looking for the <a href='https://census2011.adrianfrith.com/'>Census 2011 site</a> which has more recent information.</>} />
     {children}
     <Footer />
   </Container>
